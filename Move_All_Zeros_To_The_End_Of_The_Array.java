09ac6950389af09e4cae4e788154b2b8d3ded9de
BalajiMCSE <142647799+BalajiMCSE@users.noreply.github.com>
2025-07-07 23:18:05 +0530

    Create Move_All_Zeros_To_The_End_Of_The_Array.java
    Traverse the array with loop variable as j and i fixed as -1 until j is a non-zero element(a[j]≠0) and store like a[i+1]=a[j] and increment i (i++) and a[j]=0 (Simplified version: a[++i]=a[j] and a[j]=0).       [Note while using a[j]=0, you can minimize it with one special condition if(i≠j) { a[j]=0;}

---
+++ Move_All_Zeros_To_The_End_Of_The_Array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+         int i = -1;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != 0) {
+                nums[++i] = nums[j];
+                if (i != j) nums[j] = 0; 
+            }
+        }
+    }
+}
